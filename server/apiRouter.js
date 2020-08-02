3ae87754566e975ca507e90951bbeb548049b1a4
Adamu M. Dankore <zimmazone@yahoo.com>
2020-08-02 10:19:37 -0500

    upload set up

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -3,5 +3,7 @@ const apiRouter = express.Router();
 const userController = require('./controllers/userController');
 
 apiRouter.get('/', userController.homepage);
+apiRouter.post('/upload-audio', userController.apiUploadAudio);
+apiRouter.get('/:trackID', userController.apiGetAllAudio);
 
 module.exports = apiRouter;
