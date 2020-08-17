aa132abaefc2b861139b8bf336d352c4f94a6c5e
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-08-17 04:44:42 +0000

    Update file

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -30,51 +30,54 @@ User.prototype.cleanUp = function (type) {
     this.data.confirmPassword = '';
   }
   // GET RID OF BOGUS PROPERTIES with
-  this.data = {
-    ...(type == 'updateInfo' && { _id: this.data._id }),
-
-    ...(type == 'register' ||
-      type == 'login' ||
-      (type == 'updateInfo' && { username: this.data.username.trim().toLowerCase() })),
-
-    ...(type == 'register' || (type == 'updateInfo' && { firstName: this.data.firstName.trim() })),
-
-    ...(type == 'register' || (type == 'updateInfo' && { lastName: this.data.lastName.trim() })),
-
-    ...(type == 'register' ||
-      (type == 'updateInfo' && { email: this.data.email.trim().toLowerCase() })),
-
-    ...(type == 'register' && {
-      userCreationDate: this.data.userCreationDate,
-    }),
-
-    ...(type == 'register' && {
-      verified: false,
-    }),
-
-    ...(type == 'register' ||
-      (type == 'updateInfo' && {
+  switch (type) {
+    case 'login':
+      this.data = {
+        username: this.data.username.trim().toLowerCase(),
+        password: this.data.password,
+      };
+      break;
+    case 'updateInfo':
+      this.data = {
+        _id: this.data._id,
+        username: this.data.username.trim().toLowerCase(),
+        firstName: this.data.firstName.trim(),
+        lastName: this.data.lastName.trim(),
+        email: this.data.email.trim().toLowerCase(),
         about: {
           bio: this.data.about && this.data.about.bio ? this.data.about.bio : '',
           city: this.data.about && this.data.about.city ? this.data.about.city : '',
           musicCategory:
             this.data.about && this.data.about.musicCategory ? this.data.about.musicCategory : '',
         },
-      })),
-
-    ...(type == 'register' && {
-      avatar:
-        'https://res.cloudinary.com/my-nigerian-projects/image/upload/f_auto,q_auto/v1597076721/Others/john/default-avatar.jpg',
-    }),
-
-    ...(type == 'register' ||
-      type == 'changePassword' ||
-      (type == 'login' && {
+      };
+      break;
+    case 'register':
+      this.data = {
+        username: this.data.username.trim().toLowerCase(),
+        firstName: this.data.firstName.trim(),
+        lastName: this.data.lastName.trim(),
+        email: this.data.email.trim().toLowerCase(),
+        userCreationDate: this.data.userCreationDate,
+        verified: false,
+        avatar:
+          'https://res.cloudinary.com/my-nigerian-projects/image/upload/f_auto,q_auto/v1597076721/Others/john/default-avatar.jpg',
         password: this.data.password,
-      })),
-
-    ...(type == 'changePassword' && { _id: this.data._id }),
-  };
+        about: {
+          bio: this.data.about && this.data.about.bio ? this.data.about.bio : '',
+          city: this.data.about && this.data.about.city ? this.data.about.city : '',
+          musicCategory:
+            this.data.about && this.data.about.musicCategory ? this.data.about.musicCategory : '',
+        },
+      };
+      break;
+    case 'changePassword':
+      this.data = {
+        _id: this.data._id,
+        password: this.data.password,
+      };
+      break;
+  }
 };
 
 User.prototype.validate = function (type) {
@@ -189,6 +192,7 @@ User.prototype.register = function () {
 User.prototype.login = function () {
   return new Promise(async (resolve, reject) => {
     await this.validate();
+    this.cleanUp('login');
 
     if (!this.errors.length) {
       usersCollection
