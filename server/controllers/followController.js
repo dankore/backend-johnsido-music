c8e2e9a50968a798c53868729ad70785a3a385be
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-08-19 05:56:28 +0000

    Update file

---
+++ server/controllers/followController.js
@@ -0,0 +1,14 @@
+const Follow = require('../models/followModel');
+
+exports.apiFollowUser = (req, res) => {
+  let follow = new Follow(req.params.username, req.apiUser._id);
+
+  follow
+    .followUser()
+    .then(() => {
+      res.json(true);
+    })
+    .catch(() => {
+      res.json(false);
+    });
+};
--- server/models/followModel.js
+++ server/models/followModel.js
@@ -1,9 +1,60 @@
-let Follow = function (followedUsername, authorId) {
+const usersCollection = require('../../db').db().collection('users');
+const followsCollection = require('../../db').db().collection('follows');
+// eslint-disable-next-line prettier/prettier
+const {
+  ObjectID
+} = require('mongodb');
+
+let Follow = function (followedUsername, followerId) {
   this.followedUsername = followedUsername;
-  this.authorId = authorId;
+  this.followerId = followerId;
   this.errors = [];
 };
 
+Follow.prototype.validate = async function (type) {
+  // DON'T FOLLOW A USER IF THEY ARE NOT REGISTERED
+  const followedAccount = await usersCollection.findOne({
+    username: this.followedUsername,
+  });
+  if (followedAccount) {
+    this.followedId = followedAccount._id;
+  } else {
+    this.errors.push('This user does not exists');
+  }
+
+  // DON'T FOLLOW A USER IF THEY ARE ALREADY BEING FOLLOWED
+  const isFollowed = await followsCollection.findOne({
+    followedId: this.followedId,
+    followerId: new ObjectID(this.followerId),
+  });
+
+  if (type == 'followSomeone') {
+    if (isFollowed) {
+      this.errors.push('You are already following this user.');
+    }
+  }
+};
+
+Follow.prototype.cleanUp = function () {
+  if (typeof this.followedUsername != 'string') {
+    this.followedUsername = '';
+  }
+};
+
 Follow.prototype.followSomeone = function () {
-  //
+  return new Promise(async (resolve, reject) => {
+    this.cleanUp();
+    await this.validate('followSomeone');
+
+    // SAVE
+    if (!this.errors.length) {
+      await followsCollection.insertOne({
+        followedId: this.followedId,
+        followerId: new ObjectID(this.followerId),
+      });
+      resolve();
+    } else {
+      reject(this.errors);
+    }
+  });
 };
